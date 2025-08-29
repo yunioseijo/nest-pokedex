8a4ed56733bfaa8c10e343e78c4bedcf92bb8f89
yunioseijo <yunioseijo@gmail.com>
2025-08-29 13:27:38 +0200

    Set global prefix, instalación y configuración de swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Pokedex API')
+    .setDescription('The Pokedex API description')
+    .setVersion('1.0')
+    .addTag('pokedex')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
